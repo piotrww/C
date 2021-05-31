104f3ec152a77004998819d44d30ba7cde1b57a5
piotr.warsza <piotr.warsza@kantar.com>
2021-05-31 16:27:40 +0200

    Add project files.

---
+++ ConsoleApp1_peter_3105/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1_peter_3105
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int pm2_5 = 22;
+            Console.WriteLine("pm2,5 --> " + pm2_5);
+            Console.WriteLine( $"default(DateTime) = {default(DateTime)}");
+
+
+            string[] names;
+            names = new string[4];
+            names[1] = "Mirek";
+            names[0] = "Tolek";
+            names[3] = "Milek";
+           Console.WriteLine(names[1]);
+            Console.WriteLine(names[0]);
+
+            for (int i = 0; i <names.Length ;i++)
+            {
+                Console.WriteLine(names[i]);
+
+            };
+        }
+    }
+}
